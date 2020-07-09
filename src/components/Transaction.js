2113efa71d85759b7645170c294b8b4ce574983d
Ruma Das <rumaadl16@gmail.com>
2020-07-10 02:01:37 +0930

    adding context and reducer in project

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -13,3 +13,5 @@ export const Transaction = ({tracsaction}) => {
 
 export default Transaction;
 // Math.abs (tracn.amount is used for make it sabsolute number no negative number)
+// Tarnery operator here its means if its less then - mins less then 0 then vclass '"ninus"' or alse class "plus"  
+//tracsaction.amount < 0 ? 'minus' : 'plus'}
