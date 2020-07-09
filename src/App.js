52ff161b6e4c87e80fe472d32ceb768275943df9
Ruma Das <rumaadl16@gmail.com>
2020-07-10 00:13:21 +0930

    adding expense file

--- src/App.js
+++ src/App.js
@@ -1,45 +1,24 @@
 import React from 'react';
-import Header from "./component/Header";
-import Navbar from './component/Navbar';
-import Note from './component/Note';
-import Footer from './component/Footer';
-import AboutUs from './component/AboutUs';
-import AddNote from './component/AddNote';
+import {Header} from './components/Header';
+import {Balance} from './components/Balance';
+import {IncomeExpenses} from './components/IncomeExpenses';
+import {TranscationList} from './components/TranscationList';
+import {AddTrans} from './components/AddTrans';
 import './App.css';
 
-
- class App extends React.Component {
-   state = {
-     notes: [
-       {
-         id: 1,
-         title: "Notes 1",
-         des: " Note 1 DEscription"
-       },
-       {
-        id: 2,
-        title: "Notes 2",
-        des: " Note 2 DEscription"
-      },
-      {
-        id: 3,
-        title: "Notes 3",
-        des: " Note 3 DEscription"
-      }
-     ]
-   }
-   render(){
-    return (
-      <div className="container">
-        <Navbar />
-        <Header />
-        <Note notes={this.state.notes}/>
-        <Footer />
-
-      </div>
-    );
-   }
-
+function App() {
+  return (
+    <div>
+    <Header />
+    <div className="container">
+    <Balance/>
+    <IncomeExpenses />
+   <TranscationList/>
+   <AddTrans />
+  </div>
+    </div>
+  
+  );
 }
 
 export default App;
